e2d81d12ad7ed58f6a85c547288ebbf7453295c6
Manuel Chagoyan <mchagoyan@chusd.org>
2025-11-10 10:12:33 -0800

    Initial commit

---
+++ Poster/VSCodeExtensions/scripts.js
@@ -0,0 +1,6 @@
+// ? This is a question comment
+// ! This is an alert comment
+// TODO this is a todo comment
+// * this is an alert comment
+
+window.alert("Hello!");
---
+++ VSCodeExtensions/scripts.js
@@ -0,0 +1,7 @@
+// ? This is a question comment
+// ! This ie an alert comment
+// TODO This is a todo comment
+// * This is a regular comment
+
+
+window.alert("Hello!");
